347846c6d00b24b48e4fca6f40e471a25f8c715d
codeair <momairhussain6@gmail.com>
2025-09-22 19:00:18 +0530

    main.py now accepts text files (base project completed)& C:/Users/Omair/AppData/Local/Programs/Python/Python313/python.exe c:/College/2_jupyter/GDG/bpe_tokenizer/src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from tokenizer import encode , decode 
+from pathlib import Path
+import json
 
 def main():
-    text = 'testing longer paragraph now  In this video I explain Byte-Pair Encoding, which is the sub-word tokenization algorithm used by GPT. In other words, its an algorithm for converting text into numbers. Lets dive in.But wait, theres more! In the next video, were going to be coding up BPE in Python, from scratch. So stay tuned '
+    
+    file_path = Path(__file__).parent.parent / 'alice.txt'
+    with file_path.open('r') as f:
+        text = f.read()  #storing text file contents into text varaible
 
     encoded_ids,merges=encode(text)     #encode text into excoded_ids and save changes into merges
     print(len(encoded_ids))
@@ -10,6 +15,11 @@ def main():
     if list(map(int,text.encode('utf-8'))) == decoded_ids: print('decoding completed successfully')   #check if message was decoded perfectly
     else: print('failed decoding')
 
-
+    output_path = Path(__file__).parent.parent / 'output.txt'
+    with output_path.open('w') as f:
+        f.write('\nencoded-\n')
+        f.write(','.join(map(str, encoded_ids)))
+        f.write('\decoded-\n')
+        f.write(','.join(map(str,decoded_ids)))
 
 main()

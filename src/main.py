95dfedf3c9ad664d03ab007f7cca36f2fe518630
codeair <momairhussain6@gmail.com>
2025-09-22 19:32:39 +0530

    fixed main.py and tokenizer.py

--- src/main.py
+++ src/main.py
@@ -3,22 +3,34 @@ from pathlib import Path
 
 def main():
     
+
+
     file_path = Path(__file__).parent.parent / 'test.txt'
     with file_path.open('r') as f:
-        text = f.read()  #storing text file contents into text varaible
+        text = f.read()                                               #storing text file contents into text varaible
 
-    encoded_ids,merges=encode(text)     #encode text into excoded_ids and save changes into merges
+
+
+    encoded_ids,merges=encode(text)                                   #encode text into excoded_ids and save changes into merges
     print(len(encoded_ids))
+
+
     decoded_ids=decode(encoded_ids,merges)
     print(len(decoded_ids))
+
+
     if list(map(int,text.encode('utf-8'))) == decoded_ids: print('decoding completed successfully')   #check if message was decoded perfectly
     else: print('failed decoding')
 
-    output_path = Path(__file__).parent.parent / 'output.txt'        #getting path for output.txt
-    with output_path.open('w') as f:    #creating and writing in output.txt
+
+
+    output_path = Path(__file__).parent.parent / 'output.txt'                #getting path for output.txt
+    with output_path.open('w') as f:                                         #creating and writing in output.txt
         f.write('\n------------------encoded--------------\n')
-        f.write(','.join(map(str, encoded_ids)))            #storing encoded data
+        f.write(','.join(map(str, encoded_ids)))                             #storing encoded data
         f.write('\n-----------decoded--------------\n')
-        f.write(','.join(map(str,decoded_ids)))         #storing decoded data
+        f.write(','.join(map(str,decoded_ids)))                              #storing decoded data
+
+
 
 main()
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,7 +1,7 @@
 def get_stats(ids):
     counts = {}
-    for pair in zip(ids , ids[1:]):   #iterates through all different pairs
-        counts[pair] = counts.get(pair,0)+1  # counts number of times pair appeared
+    for pair in zip(ids , ids[1:]):                                     #iterates through all different pairs
+        counts[pair] = counts.get(pair,0)+1                             # counts number of times pair appeared
     return counts
 
 
@@ -9,45 +9,59 @@ def merge(ids,pair,idx):
     newids = []
     i=0
     while i<len(ids):
-        if i<len(ids)-1 and ids[i] ==pair[0] and ids[i+1] == pair[1]:  #checks if pair matches through the sequence
-            newids.append(idx)   #puts idx instead of the pair and increments the byte by 2 to skip the pairs
+        if i<len(ids)-1 and ids[i] ==pair[0] and ids[i+1] == pair[1]:    #checks if pair matches through the sequence
+            newids.append(idx)                                        #puts idx instead of the pair and increments the byte by 2 to skip the pair indexes
             i=i+2
         else:
-            newids.append(ids[i])  #pair did not match, hence placing the orignal byte and incrementing 1
+            newids.append(ids[i])                                 #pair did not match, hence placing the orignal byte and incrementing 1
             i=i+1
     ids= newids
     return ids
 
 def encode(text):
+
     merges = {}
     ids = text.encode('utf-8')
-    ids = list(map(int,ids)) #converting bytes to int values
-    idx= 256 #since all int values from 0 to 255 are already taken
+    ids = list(map(int,ids))                                          #converting bytes to int values
+    idx= 256                                                            #all int values from 0 to 255 are not available
+    
+                                                    
     while True:
-        stats = get_stats(ids)  #finding pairs and how many times they appear
+
+        stats = get_stats(ids)                                       #finding pairs and how many times they appear
         if not stats:
             break;
-        pair = max(stats, key=stats.get) #finding current max occurance of pair
+        pair = max(stats, key=stats.get)                                #finding current max occurance of pair
         if stats[pair]  <=1:
-            break
+            break                                                       #leave the loop only when all pairs are unique in the ids
         #print(f'merging {pair} into {idx}')        
-        ids = merge(ids,pair,idx)   #merging the pairs of byte into a single byte
-        merges[pair] = idx  #noting the byte value stored at pair's position
+        ids = merge(ids,pair,idx)                                           #merging the pairs of byte into a single byte
+        merges[pair] = idx                                            #noting the byte value stored at pair's position
         idx =idx+1
-    return ids,merges         #return the new token and changes made in it stored in merges
+
+
+
+    return ids,merges                                                    #return the new token and changes made in it stored in merges
 
 
 def decode(ids,merges):
-    for pair in reversed(merges):           #iterating through all merges completed during encoding in reverse order
+
+    for pair in reversed(merges):                                         #iterating through all merges completed during encoding in reverse order
         new_ids = []
-        last_token = merges[pair]  #getting the last token
+        last_token = merges[pair]                                        #getting the last token
         i=0
-        while i<len(ids):              #checking all bytes to find the token to convert to the pair
+
+
+
+        while i<len(ids):                                                #checking all bytes to find the token to convert to the pair
             if ids[i] == last_token:
-                new_ids.extend(pair)  #replacing the token with pair
+                new_ids.extend(pair)                                      #replacing the token with pair
                 i=i+1
             else:
-                new_ids.append(ids[i])  #putting back the byte in place
+                new_ids.append(ids[i])                                    #putting back the byte in place
                 i=i+1
         ids = new_ids
+
+
+
     return ids
